7141798cb671fafbad8be7dde3f3eef414d48996
Joon-Young <jgwak1@binghamton.edu>
2025-12-22 17:48:01 -0500

    pytorch transformer - attention class forward

--- src/scratch_implementations/pytorch_transformer.py
+++ src/scratch_implementations/pytorch_transformer.py
@@ -17,7 +17,54 @@ class CausalSelfAttentionPyTorch(nn.Module):
         # 2. Output Projection Weights
         self.c_proj = nn.Linear(d_model, d_model)
 
-
-    # TODO
     def forward(self, x):
-       pass
+        # x shape: [Batch, Time, Channel] (B, T, C)
+        B, T, C = x.size()
+
+        # 1. LINEAR PROJECTION
+        # [B, T, C] @ [C, 3*C] -> [B, T, 3*C]
+        qkv = self.c_attn(x)
+
+        # 2. SPLIT Q, K, V
+        # Split last dim into 3 parts. Result: 3 tensors of [B, T, C]
+        q, k, v = qkv.split(self.d_model, dim=2)
+
+        # 3. RESHAPE & TRANSPOSE (The "Permute" in PyTorch)
+        # Reshape: [B, T, n_head * head_dim] -> [B, T, n_head, head_dim]
+        # Permute: Swap Time(1) and Head(2)  -> [B, n_head, T, head_dim]
+        q = q.view(B, T, self.n_head, self.head_dim).permute(0, 2, 1, 3)
+        k = k.view(B, T, self.n_head, self.head_dim).permute(0, 2, 1, 3)
+        v = v.view(B, T, self.n_head, self.head_dim).permute(0, 2, 1, 3)
+
+        # 4. SCALED DOT-PRODUCT ATTENTION
+        # (B, h, T, hs) @ (B, h, hs, T) -> (B, h, T, T)
+        att = (q @ k.transpose(-2, -1)) * self.scale
+
+        # 5. CAUSAL MASKING
+        mask = torch.tril(torch.ones(T, T, device=x.device))
+        
+        # Apply Mask: Where mask is 0, set position to -inf.
+        att = att.masked_fill(mask == 0, float('-inf'))
+        
+        att = F.softmax(att, dim=-1)
+
+        # 6. AGGREGATE for Attention Outputs
+        # (B, h, T, T) @ (B, h, T, hs) -> (B, h, T, hs)
+        y = att @ v
+
+        # 7. REASSEMBLE HEADS
+        # Permute: [B, h, T, hs] -> [B, T, h, hs]
+        # Reshape: [B, T, h, hs] -> [B, T, C] (where C = n_head * head_dim)
+        y = y.permute(0, 2, 1, 3).contiguous().view(B, T, C)
+
+        # 8. OUTPUT PROJECTION
+        # [B, T, C] @ [C, C] -> [B, T, C]
+        return self.c_proj(y)
+
+
+
+class MLPPyTorch(nn.Module):
+    
+    # TODO    
+    def __init__(self, d_model=512):
+        pass
